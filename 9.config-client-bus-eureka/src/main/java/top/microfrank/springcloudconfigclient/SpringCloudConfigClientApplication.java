ed192e93711b41f88d391ed618927c8e8b476356
Frank <sunwu51@126.com>
2018-01-12 02:23:56 +0800

    add config-client-bus-eureka

---
+++ 9.config-client-bus-eureka/src/main/java/top/microfrank/springcloudconfigclient/SpringCloudConfigClientApplication.java
@@ -0,0 +1,28 @@
+package top.microfrank.springcloudconfigclient;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableEurekaClient
+@RestController
+@RefreshScope
+public class SpringCloudConfigClientApplication {
+
+	@Value("${profile}")
+	String proflie;
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudConfigClientApplication.class, args);
+	}
+	@GetMapping("/")
+	public String index(){
+		return proflie;
+	}
+
+}
